f8b3ddf422d5a08c4d0acb7b9de7bcf4b63d565b
Nikos Karampas <nikos.karampas@hotmail.com>
2022-10-07 16:59:40 +0300

    Added Factory exercise

---
+++ Factory/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var person1 = new PersonFactory();
+person1.CreatePerson("Nick");
+
+var person2 = new PersonFactory();
+person2.CreatePerson("George");
+
+var person3 = new PersonFactory();
+person3.CreatePerson("Michael");
+
+Console.WriteLine(person1);
+Console.WriteLine(person2);
+Console.WriteLine(person3);
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public override string ToString()
+    {
+        string newPersonObj = "Id: " + Id.ToString() + " Name: " + Name;
+        return newPersonObj;
+    }
+}
+  
+public class PersonFactory
+{
+    private static int id = 0;
+    private Person person;
+    public Person CreatePerson(string name){
+        person = new Person(){
+            Id = id++,
+            Name = name
+        };
+        return person;
+    }
+    public override string ToString()
+    {
+        return person.ToString();
+    }
+}
