6da59811742f62f147cdcc3b2f14ab72e7c629cc
Nikos Karampas <nikos.karampas@hotmail.com>
2022-10-14 16:24:29 +0300

    Adapter design pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,37 @@
+﻿var square = new Square{Side = 11};
+var squareToRectangle = new SquareToRectangleAdapter(square);
+bool result = ExtensionMethods.Area(squareToRectangle) == 121;
+
+Console.WriteLine("Area is 121: " + result.ToString());
+
+public class Square
+{
+    public int Side;
+}
+
+public interface IRectangle
+{
+    int Width { get; }
+    int Height { get; }
+}
+
+public static class ExtensionMethods
+{
+    public static int Area(this IRectangle rc)
+    {
+        return rc.Width * rc.Height;
+    }
+}
+
+public class SquareToRectangleAdapter : IRectangle
+{
+    private readonly Square square;
+
+    public SquareToRectangleAdapter(Square square)
+    {
+    this.square = square;
+    }
+
+    public int Width => square.Side;
+    public int Height => square.Side;
+}
