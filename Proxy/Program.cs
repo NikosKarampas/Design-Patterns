57064798558c3611357b5ecebf1edfba1ba8a3aa
Nikos Karampas <nikos.karampas@hotmail.com>
2022-10-19 15:06:26 +0300

    Proxy design pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,59 @@
+﻿var person = new Person();
+person.Age = 17;
+
+var responsiblePerson = new ResponsiblePerson(person);
+Console.WriteLine(responsiblePerson.Drink());
+Console.WriteLine(responsiblePerson.Drive());
+Console.WriteLine(responsiblePerson.DrinkAndDrive());
+
+public class Person
+{
+    public int Age { get; set; }
+
+    public string Drink()
+    {
+        return "drinking";
+    }
+
+    public string Drive()
+    {
+        return "driving";
+    }
+
+    public string DrinkAndDrive()
+    {
+        return "driving while drunk";
+    }
+}
+
+public class ResponsiblePerson
+{
+    private readonly Person person;
+    public ResponsiblePerson(Person person)
+    {
+        this.person = person;
+    }
+
+    public int Age 
+    { 
+        get { return person.Age; }
+        set { person.Age = value; }
+    }
+
+    public string Drink()
+    {
+        if (Age < 18)
+            return "too young";
+        return person.Drink();
+    }
+
+    public string Drive(){
+        if (Age < 16)
+            return "too young";
+        return person.Drive();
+    }
+
+    public string DrinkAndDrive(){
+        return "dead";
+    }
+}
