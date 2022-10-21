d536ae60a1abe13ac14579a63d0d9851d3552450
Nikos Karampas <nikos.karampas@hotmail.com>
2022-10-21 13:39:22 +0300

    Iterator design pattern

---
+++ Iterator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+Console.WriteLine("test");
+
+var creature = new Creature();
+creature.Strength = 3;
+creature.Agility = 4;
+creature.Intelligence = 5;
+
+foreach (var cr in creature)
+{
+  Console.WriteLine(cr);
+}
+
+Console.WriteLine(creature[0]);
+
+public class Creature : IEnumerable<int>
+{
+  private int [] stats = new int[3];
+
+  private const int strength = 0;
+  private const int agility = 1;
+  private const int intelligence = 2;
+
+  public int Strength
+  {
+    get => stats[strength];
+    set => stats[strength] = value;
+  }
+
+  public int Agility
+  {
+    get => stats[agility];
+    set => stats[agility] = value;
+  }
+
+  public int Intelligence
+  {
+    get => stats[intelligence];
+    set => stats[intelligence] = value;
+  }
+
+  public double AverageStat => 
+    stats.Average();
+
+  public IEnumerator<int> GetEnumerator()
+  {
+    return stats.AsEnumerable().GetEnumerator();
+  }
+
+  IEnumerator IEnumerable.GetEnumerator()
+  {
+    return GetEnumerator();
+  }
+
+  public int this[int index]
+  {
+    get { return stats[index]; }
+    set { stats[index] = value; }
+  }
+}
